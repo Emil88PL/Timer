d6ff5411dcb7aeaefaad7de512de5c093a7378da
Emil <emil88pl@gmail.com>
2023-07-01 12:08:27 +0100

    Timer 2

--- src/App.java
+++ src/App.java
@@ -12,23 +12,31 @@ public class App {
 
         TimerTask task = new TimerTask(){
 
+            int counter = 10;
             @Override
             public void run() {
-                System.out.println("Count down completed!!!!");
+                if (counter > 0) {
+                    System.out.println(counter + " seconds");
+                    counter--;
+                } else {
+                    System.out.println("Suprise!");
+                    timer.cancel();
+                }
             }   
         };
 
-        Calendar date = Calendar.getInstance();
-        date.set(Calendar.YEAR, 2023);
-        date.set(Calendar.MONTH, Calendar.JULY);
-        date.set(Calendar.DAY_OF_MONTH, 1);
-        date.set(Calendar.HOUR_OF_DAY, 12);
-        date.set(Calendar.MINUTE, 2);
-        date.set(Calendar.SECOND, 0);
-
+        // Calendar date = Calendar.getInstance();
+        // date.set(Calendar.YEAR, 2023);
+        // date.set(Calendar.MONTH, Calendar.JULY);
+        // date.set(Calendar.DAY_OF_MONTH, 1);
+        // date.set(Calendar.HOUR_OF_DAY, 12);
+        // date.set(Calendar.MINUTE, 2);
+        // date.set(Calendar.SECOND, 0);
 
         //timer.schedule(task, 3000);
-        timer.schedule(task,date.getTime());
+       // timer.schedule(task,date.getTime());
+
+       timer.scheduleAtFixedRate(task, 0, 1000);
 
     }
 }
