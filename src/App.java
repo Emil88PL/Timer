e1d6d365e3eca76f4bcdfda4036bec2c033f98a7
Emil <emil88pl@gmail.com>
2023-07-01 12:13:27 +0100

    Timer from

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         
-        System.out.println("Task started....");
+       // System.out.println("Task started....");
 
         Timer timer = new Timer();
 
@@ -25,18 +25,18 @@ public class App {
             }   
         };
 
-        // Calendar date = Calendar.getInstance();
-        // date.set(Calendar.YEAR, 2023);
-        // date.set(Calendar.MONTH, Calendar.JULY);
-        // date.set(Calendar.DAY_OF_MONTH, 1);
-        // date.set(Calendar.HOUR_OF_DAY, 12);
-        // date.set(Calendar.MINUTE, 2);
-        // date.set(Calendar.SECOND, 0);
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.YEAR, 2023);
+        date.set(Calendar.MONTH, Calendar.JULY);
+        date.set(Calendar.DAY_OF_MONTH, 1);
+        date.set(Calendar.HOUR_OF_DAY, 12);
+        date.set(Calendar.MINUTE, 12);
+        date.set(Calendar.SECOND, 50);
 
         //timer.schedule(task, 3000);
        // timer.schedule(task,date.getTime());
-
-       timer.scheduleAtFixedRate(task, 0, 1000);
+    //    timer.scheduleAtFixedRate(task, 0, 1000);
+    timer.scheduleAtFixedRate(task, date.getTime(), 1000);
 
     }
 }
